220fecd6c95d764dbb6160b0ef63116b477ad779
ronald <ronaldlindev@gmail.com>
2024-03-11 03:50:19 -0700

    still debugging

--- util/imageProcessing.py
+++ util/imageProcessing.py
@@ -10,7 +10,7 @@ def process(image): # turns image into a np array
     # image = tf.io.read_file(fileName)  # Read the image file
     # image = tf.image.decode_image(image, channels=3)  # Decode the image
     image = tf.convert_to_tensor(image)
-    print(image.shape)
+    print(tf.shape(image))
     image = tf.image.resize(image, size = (200,100))  # Resize the image
     return image
 
